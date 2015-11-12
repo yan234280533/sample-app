44c5c6c698993eff733d76609175bfd756203194
István Földházi <istvan.foldhazi@gmail.com>
2015-11-12 16:07:10 +0100

    first commit

---
+++ src/main/java/hu/bankmonitor/demo/sampleapp/SampleController.java
@@ -0,0 +1,16 @@
+package hu.bankmonitor.demo.sampleapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@RequestMapping("/status")
+	@SuppressWarnings("static-method")
+	public String status() {
+
+		return "OK";
+	}
+
+}
